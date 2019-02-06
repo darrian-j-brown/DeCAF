6c0b3088a02649837de8716656265fbc5bbe9892
Darrian <darrian.jordan.brown@gmail.com>
2019-02-06 00:37:40 +0000

    Added getAngleDegrees function API

--- index.js
+++ index.js
@@ -15,6 +15,14 @@
         },
         radiansToDegrees(radians) {
           return radians * 180 / Math.PI;  
+        },
+        getAngleDegrees(pointA, pointB){
+          const
+          distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          radians = Math.atan2(distanceY, distanceX),
+          degrees = radians * 180 / Math.PI;
+          return degrees;
         }
       
     },
